786302dc57d3a69a603fe532e3e9bbadabbb497d
DavidCraftDev <david@davidcraft.de>
2022-02-08 15:41:19 +0100

    Initial commit

---
+++ src/main/java/dev/sancraft/antigamemode/main.java
@@ -0,0 +1,21 @@
+package dev.sancraft.antigamemode;
+
+import dev.sancraft.antigamemode.Listener.gamemodeChangeListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§aAntiGamemode by Sancraft is now enabled!");
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new gamemodeChangeListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("§cAntiGamemode by Sancraft is now disabled!");
+    }
+}
